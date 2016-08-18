8ec83797206f4c98318d6bc47d6b276ba19e517b
Aaron Craig <me@aaroncraig.com>
2016-08-18 16:30:57 -0700

    Fix build for Ember 2

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import('components/growl-manager.css');
+    const versionString = app.project.pkg.dependencies['ember-cli'];
+    const version = versionString.replace(/[^\d\.]/, '');
+
+    if (version.match(/^1/)) {
+      app.import('components/growl-manager.css');
+    } else {
+      app.import('app/styles/components/growl-manager.css');
+    }
   }
 };
