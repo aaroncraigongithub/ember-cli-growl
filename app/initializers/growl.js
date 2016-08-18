bddb7668d46610f208e2c054ec3643a2b321a87e
Aaron Craig <me@aaroncraig.com>
2016-08-18 16:50:29 -0700

    remove deprecation without breaking Ember 1 apps

--- app/initializers/growl.js
+++ app/initializers/growl.js
@@ -2,10 +2,15 @@ import Growl from '../services/growl';
 
 export default {
   name: 'growl',
-  initialize: function(app) {
-    Growl.reopenClass({
-      container: container
-    });
+  initialize: function() {
+    const app        = arguments[1] || arguments[0];
+    const controller = (arguments.length === 2) ? arguments[0] : null;
+
+    if (controller) {
+      Growl.reopenClass({
+        container: container
+      });
+    }
 
     app.register('growl:main', Growl);
     app.inject('route', 'growl', 'growl:main');
