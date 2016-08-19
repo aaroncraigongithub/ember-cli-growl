290afda69abf65925164fab25ceab0a7880e5908
Aaron Craig <me@aaroncraig.com>
2016-08-18 17:11:37 -0700

    style of variable declaration

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    const versionString = app.project.pkg.dependencies['ember-cli'];
-    const version = versionString.replace(/[^\d\.]/, '');
+    var versionString = app.project.pkg.dependencies['ember-cli'];
+    var version = versionString.replace(/[^\d\.]/, '');
 
     if (version.match(/^1/)) {
       app.import('components/growl-manager.css');
